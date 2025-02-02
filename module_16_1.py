2f722d665d12856439c74804d07172e216eb04f2
Anastasiya <165375443+anastasiya-red@users.noreply.github.com>
2025-02-02 20:15:13 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -16,12 +16,12 @@ async def welcome_admin():
 
 
 @app.get('/user/{user_id}')
-async def welcome_user(user_id):
+async def user_number(user_id):
     return f'Вы вошли как пользователь № {user_id}'
 
 
 @app.get('/user')
-async def welcome_user(username: str = 'An', age: int = 34):
+async def user_info(username: str = 'An', age: int = 34):
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
 
 
